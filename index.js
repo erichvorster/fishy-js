fc15b9c1bff3199bedfe0b29dd45a59a6cbcf229
erichvorster <erichvorster1@gmail.com>
2022-05-11 18:31:35 +0200

    1st commit

---
+++ index.js
@@ -0,0 +1,24 @@
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+
+      //Leaflet code - L is the namespace
+      const map = L.map("map").setView([longitude, latitude], 13);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([longitude, latitude])
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
+    },
+    function () {
+      alert("Could not find you");
+    }
+  );
+}
